08706a5f0a8312377603814839666590e42b7bd9
nhutmd <doanminhnhut.vn@gmail.com>
2019-08-02 12:36:22 +0700

    tidy js

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const puppeteer = require('puppeteer');
+
 const uname = '';
 const pwd = '';
 const project = '/projects/dc_general?jump=my';
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  page.setViewport({ width:1366, height:768 });
-  await page.goto('http://192.168.0.16:85/login');
-  await page.type('#username', uname);
-  await page.type('#password', pwd);
-  await page.click('[name="login"]')
+(async() => {
+
+	const browser = await puppeteer.launch({
+		headless: false
+  });
   
-  await page.waitFor('#project_quick_jump_box');
-  await page.select('#project_quick_jump_box', project)
-  await page.$eval('#project_quick_jump_box', el => el.onchange());
+	const page = await browser.newPage();
+	page.setViewport({
+		width: 1366,
+		height: 768
+  });
   
-  await page.waitFor('[href="/projects/dc_general/time_entries/new"]');
-  await page.$eval('[href="/projects/dc_general/time_entries/new"]', el => el.click());
-  
-  await page.waitFor('#time_entry_spent_on');
-  await page.waitFor('#time_entry_hours');
-  await page.waitFor('#time_entry_activity_id');
-  await page.waitFor('#new_time_entry');
-  await page.type('#time_entry_hours', "8")
-  await page.select('#time_entry_activity_id', "33")
+	await page.goto('http://192.168.0.16:85/login');
+	await page.type('#username', uname);
+	await page.type('#password', pwd);
+	await page.click('[name="login"]')
+
+	await page.waitFor('#project_quick_jump_box');
+	await page.select('#project_quick_jump_box', project)
+	await page.$eval('#project_quick_jump_box', el => el.onchange());
+
+	await page.waitFor('[href="/projects/dc_general/time_entries/new"]');
+	await page.$eval('[href="/projects/dc_general/time_entries/new"]', el => el.click());
+
+	await page.waitFor('#time_entry_spent_on');
+	await page.waitFor('#time_entry_hours');
+	await page.waitFor('#time_entry_activity_id');
+	await page.waitFor('#new_time_entry');
+	await page.type('#time_entry_hours', "8")
+	await page.select('#time_entry_activity_id', "33")
   await page.$eval('#new_time_entry', el => el.submit());
-  //await browser.close();
+  
+	await browser.close();
 })();
